b797f6469e58073d1494bf6489a5cf3813f61092
Arlo O'Keeffe <a.okeeffe@sprinteins.com>
2024-12-02 13:37:32 +0100

    add tcp client

--- Program.cs
+++ Program.cs
@@ -1,11 +1,48 @@
-﻿try
+﻿using System.Net.Sockets;
+using System.Text;
+
+try
 {
-    var client = new HttpClient();
-    var responseBody = await client.GetStringAsync("http://127.0.0.1:52126");
+    string host = "127.0.0.1";
+    int port = 52126;
+    string endpoint = "/";
+
+    var httpClient = new HttpClient();
+    var responseBody = await httpClient.GetStringAsync("http://" + host + ":" + port + endpoint);
     Console.WriteLine(responseBody);
+
+    using (var client = new TcpClient(host, port))
+    {
+        using (var networkStream = client.GetStream())
+        {
+            // Prepare the HTTP GET request
+            string request = $"GET {endpoint} HTTP/1.1\r\n" +
+                                $"Host: {host}\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n";
+            byte[] requestBytes = Encoding.ASCII.GetBytes(request);
+
+            // Send the HTTP request
+            networkStream.Write(requestBytes, 0, requestBytes.Length);
+
+            // Read the server response
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            StringBuilder responseBuilder = new StringBuilder();
+
+            while ((bytesRead = networkStream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                string responsePart = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                responseBuilder.Append(responsePart);
+            }
+
+            // Output the response
+            Console.WriteLine("Server Response:");
+            Console.WriteLine(responseBuilder.ToString());
+        }
+    }
 }
-catch (HttpRequestException e)
+catch (Exception ex)
 {
-    Console.WriteLine("\nException Caught!");
-    Console.WriteLine("Message :{0} ", e.Message);
+    Console.WriteLine($"An error occurred: {ex.Message}");
 }
